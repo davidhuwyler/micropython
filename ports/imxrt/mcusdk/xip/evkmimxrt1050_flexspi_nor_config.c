c8c2596f54350b7bb322fb646c50815bdd5a621e
dave <david.huwyler@gmail.com>
2019-10-09 18:47:32 +0200

    added xip for qsip flash

--- ports/imxrt/mcusdk/xip/evkmimxrt1050_flexspi_nor_config.c
+++ ports/imxrt/mcusdk/xip/evkmimxrt1050_flexspi_nor_config.c
@@ -31,42 +31,40 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "fsl_flexspi_nor_boot.h"
+#include <xip/evkmimxrt1050_flexspi_nor_config.h>
 
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
 #if defined(XIP_BOOT_HEADER_ENABLE) && (XIP_BOOT_HEADER_ENABLE == 1)
 #if defined(__CC_ARM) || defined(__GNUC__)
-    __attribute__((section(".boot_hdr.ivt")))
+__attribute__((section(".boot_hdr.conf")))
 #elif defined(__ICCARM__)
-#pragma location=".boot_hdr.ivt"
+#pragma location = ".boot_hdr.conf"
 #endif
-/************************************* 
- *  IVT Data 
- *************************************/
-const ivt image_vector_table = {
-  IVT_HEADER,                         /* IVT Header */
-  IMAGE_ENTRY_ADDRESS,                /* Image Entry Function */
-  IVT_RSVD,                           /* Reserved = 0 */
-  (uint32_t)DCD_ADDRESS,              /* Address where DCD information is stored */
-  (uint32_t)BOOT_DATA_ADDRESS,        /* Address where BOOT Data Structure is stored */
-  (uint32_t)&image_vector_table,      /* Pointer to IVT Self (absolute address */
-  (uint32_t)CSF_ADDRESS,              /* Address where CSF file is stored */
-  IVT_RSVD                            /* Reserved = 0 */
+
+const flexspi_nor_config_t qspiflash_config = {
+    .memConfig =
+        {
+            .tag = FLEXSPI_CFG_BLK_TAG,
+            .version = FLEXSPI_CFG_BLK_VERSION,
+            .readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackFromDqsPad,
+            .csHoldTime = 3u,
+            .csSetupTime = 3u,
+            // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
+            .sflashPadType = kSerialFlash_4Pads,
+            .serialClkFreq = kFlexSpiSerialClk_100MHz,
+            .sflashA1Size = 8u * 1024u * 1024u,
+            .lookupTable =
+                {
+                    // Read LUTs
+                    FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xEB, RADDR_SDR, FLEXSPI_4PAD, 0x18),
+                    FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_4PAD, 0x06, READ_SDR, FLEXSPI_4PAD, 0x04),
+                },
+        },
+    .pageSize = 256u,
+    .sectorSize = 4u * 1024u,
+    .blockSize = 256u * 1024u,
+    .isUniformBlockSize = false,
 };
-
-#if defined(__CC_ARM) || defined(__GNUC__)
-    __attribute__((section(".boot_hdr.boot_data")))
-#elif defined(__ICCARM__)
-#pragma location=".boot_hdr.boot_data"
-#endif
-/************************************* 
- *  Boot Data 
- *************************************/
-const BOOT_DATA_T boot_data = {
-  FLASH_BASE,                 /* boot start location */
-  FLASH_SIZE,                 /* size */
-  PLUGIN_FLAG,                /* Plugin flag*/
-  0xFFFFFFFF  				  /* empty - extra data word */
-};
-#endif
-
-
+#endif /* XIP_BOOT_HEADER_ENABLE */
