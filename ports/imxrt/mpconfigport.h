0ae717b9b58f336061f623617d7889dff2f6cc37
dave <david.huwyler@gmail.com>
2019-10-18 11:40:58 +0200

    activated micropython sensor module

--- ports/imxrt/mpconfigport.h
+++ ports/imxrt/mpconfigport.h
@@ -168,7 +168,7 @@ extern const struct _mp_obj_module_t mp_module_network;
 extern const struct _mp_obj_module_t time_module;
 //extern const struct _mp_obj_module_t lcd_module;
 #ifndef OMV_MPY_ONLY
-//extern const struct _mp_obj_module_t sensor_module;
+extern const struct _mp_obj_module_t sensor_module;
 //extern const struct _mp_obj_module_t image_module;
 //extern const struct _mp_obj_module_t mjpeg_module;
 //extern const struct _mp_obj_module_t nn_module;
@@ -224,6 +224,7 @@ extern const struct _mp_obj_module_t time_module;
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_uos), (mp_obj_t)&mp_module_uos }, \
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_utime), (mp_obj_t)&mp_module_utime }, \
 	{ MP_OBJ_NEW_QSTR(MP_QSTR_time), (mp_obj_t)&time_module }, \
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_sensor),  (mp_obj_t)&sensor_module }, \
 	SOCKET_BUILTIN_MODULE \
 	NETWORK_BUILTIN_MODULE
 #else
